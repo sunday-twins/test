25fc24a94c6439c478edcd8d2db871b39738db96
jihyekim <jjasmint@naver.com>
2025-01-19 23:00:25 +0900

    sub1 생성

---
+++ react-01/src/components/list/box_profile.jsx
@@ -0,0 +1,14 @@
+import '../../scss/components/list/profile.scss'
+
+const BoxProfile = ({ src, name }) => {
+  return (
+    <div className="box_profile">
+      <div className="img">
+        <img src={src} alt="" />
+      </div>
+      <div className="name">{name}</div>
+    </div>
+  )
+};
+
+export default BoxProfile;
--- react-01/src/pages/sub1.jsx
+++ react-01/src/pages/sub1.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
+import BoxProfile from "../components/list/box_profile";
+
 const Sub1 = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list")
+      .then((response) => response.json())
+      .then((data) => setData(data.slice(0, 20)))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
   return (
-    <section className="page_sub">
+    <div className="page_sub">
       <div className="inner">
         <h2>sub 1</h2>
+
+        <article className="box_thumb_profile">
+          <h3>대표 이미지</h3>
+          <BoxProfile src="https://picsum.photos/seed/picsum/200/300" name="name" />
+        </article>
+
+        <section className="section">
+          <h3>author</h3>
+          <ul className="list_img_name">
+            {data.map((d, index) => (
+              <li className="items_profile" key={index}>
+                <BoxProfile src={d.download_url} name={d.author} />
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
-    </section>
+    </div>
   )
 };
 

62363cd231de0a4347cd2057e18bcb6760424b92
jihyekim <jjasmint@naver.com>
2025-01-19 23:37:53 +0900

    버튼 추가

---
+++ react-01/src/mock/mock_sub3.jsx
@@ -0,0 +1,12 @@
+const SUB3_BUTTON = [
+  {
+    class: 'btn_common',
+    button: '취소'
+  },
+  {
+    class: 'btn_common type_point',
+    button: '확인'
+  },
+];
+
+export { SUB3_BUTTON };
--- react-01/src/pages/sub3.jsx
+++ react-01/src/pages/sub3.jsx
@@ -1,10 +1,43 @@
+import '../scss/components/button.scss'
+import { SUB3_BUTTON } from "../mock/mock_sub3";
+
 const Sub3 = () => {
   return (
     <section className="page_sub">
       <div className="inner">
         <h2>sub 3</h2>
+
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px"
+        }}>
+
+          <h3 style={{
+            marginTop: "10px",
+            paddingTop: "10px",
+            borderTop: "1px solid #eee",
+          }}>button</h3>
+
+          <div className="box_common_btn">
+            <button type="button" className="btn_common">취소</button>
+            <button type="button" className="btn_common type_point">확인</button>
+          </div>
+
+          <div className="box_common_btn">
+            <button type="button" className="btn_common" disabled>취소</button>
+            <button type="button" className="btn_common type_point" disabled>확인</button>
+          </div>
+
+          <div style={{
+            display: "flex",
+          }}>
+            <a href="#" className="btn_download">download</a>
+          </div>
+
+        </div>
       </div>
-    </section>
+    </section >
   )
 };
 
